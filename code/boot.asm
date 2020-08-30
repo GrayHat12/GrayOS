557325a804c39b9e91dbfcfaeaec17efdbfbc15e
GrayHat12 <grayhathacks10@gmail.com>
2020-08-30 20:07:38 +0530

    ep2

--- code/boot.asm
+++ code/boot.asm
@@ -1,4 +1,6 @@
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
 jmp $
-
-times 510-($-$$) db 0
-db 0x55, 0xaa
+times 510-($-$$) db 0 ; this lines fills all remaining blocks with 0 (kinda like padding) 
+db 0x55, 0xaa ; this is the magical number which tells that the code is for boot sector
